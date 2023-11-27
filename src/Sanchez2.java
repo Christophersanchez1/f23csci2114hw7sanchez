5dc9ee1bb2e7f0c8c63392ab72a30d07c7a963f8
Christophersanchez1 <122641788+Christophersanchez1@users.noreply.github.com>
2023-11-27 10:23:36 -0600

    2

---
+++ src/Sanchez2.java
@@ -0,0 +1,27 @@
+
+public class Sanchez2 {
+    public static void main(String[] args) {
+        double squaresSum = 0;
+        int  squaredSum = 0;
+        int difference = 0;
+
+        for(int i = 0; i<=100;i++){
+            squaredSum += i;
+
+
+        }
+        squaredSum = squaredSum * squaredSum;
+
+        for(double i = 0; i<=100;i++){
+            double a =0;
+            a = Math.pow(i,2);
+            squaresSum =  squaresSum+a;
+        }
+      difference = (int) (squaredSum - squaresSum);
+        System.out.println(difference);
+        System.out.println(squaresSum);
+        System.out.println(squaredSum);
+
+
+    }
+}
