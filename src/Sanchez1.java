e4f448e030ab54a8a36720ea3f5139f3eaaa867a
Christophersanchez1 <122641788+Christophersanchez1@users.noreply.github.com>
2023-11-26 22:47:37 -0600

    i

---
+++ src/Sanchez1.java
@@ -0,0 +1,18 @@
+//4613732
+public class Sanchez1 {public static void main(String[] args) {
+    int sum = 2;
+    int n1=1;
+    int n2=2;
+    int n=0;
+
+    while(n<4000000){
+        n=n1+n2;
+        n1=n2;
+        n2=n;
+        if(n%2==0){
+            sum=sum+n;
+        }
+    }
+    System.out.println(sum);
+}
+}
