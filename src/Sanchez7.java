9a848a0f2fe19f5473eb9a60f1ca8f545c9332f2
Christophersanchez1 <122641788+Christophersanchez1@users.noreply.github.com>
2023-11-30 21:34:09 -0600

    2

---
+++ src/Sanchez7.java
@@ -0,0 +1,18 @@
+public class Sanchez7 {
+    public static void main(String[] args) {
+        for( long i =1; i<1000; i++){
+            for (long j = 2; j<1000; j++){
+                for(long l = 3; l<1000;l++){
+                    if(Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(l, 2)){
+                        long sum = i+j+l;
+                        if(sum ==1000){
+                            long product = i*j*l;
+                            System.out.println(product);
+                        }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
---
+++ src/Sanchez9.java
@@ -0,0 +1,24 @@
+public class Sanchez9 {
+    static long number= 0;
+    public static void main(String[] args) {
+        for(long i= 0; i<2000000;  i++){
+            if(isPrime(i)){
+                number = number+i;
+            }
+        }
+        System.out.println(number);
+    }
+
+    public static  boolean isPrime(long number){
+        if(number<=1){
+            return false;
+        }
+        for(int i =2;i<= Math.sqrt(number);i++){
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
