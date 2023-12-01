7251e84e61191a618b9b0f0bacc311ecf7c7e2df
Christophersanchez1 <122641788+Christophersanchez1@users.noreply.github.com>
2023-11-30 22:14:47 -0600

    2

---
+++ src/Sanchez10.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class Sanchez10 {
+    public static void main(String[] args) {
+        BigInteger power = new BigInteger("2");
+        BigInteger number = power.pow(1000);
+
+        String numberStr = number.toString();
+        long Sum = 0;
+            for (int i = 0; i< numberStr.length(); i++) {
+                Sum += Character.getNumericValue(numberStr.charAt(i));
+            }
+
+        System.out.println(Sum);
+        }
+
+    }
+
